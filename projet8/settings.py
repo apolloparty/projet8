58c587f133a82c07bacd05424d3018fcda1b4b93
apolloparty <apolloparty@gmail.com>
2020-09-09 19:34:43 +0200

    staticfiles modifyed for heroku

--- projet8/settings.py
+++ projet8/settings.py
@@ -127,10 +127,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.9/howto/static-files/
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = [
-    "/home/fostin/delivery/pythonorienté/projet-dj/projet8/static"
-]
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 CRISPY_TEMPLACE_PACK = 'boostrap4'

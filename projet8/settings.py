e83471f8a6b6cbe6b2aaaa776c9d5ba8bd381c18
apolloparty <apolloparty@gmail.com>
2020-09-09 22:03:53 +0200

    modifying client.html redirections

--- projet8/settings.py
+++ projet8/settings.py
@@ -85,7 +85,8 @@ DATABASES = {
         'PORT': '5432',
     }
 }
-
+import dj_database_url
+DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators

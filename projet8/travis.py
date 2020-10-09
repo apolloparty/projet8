f237c20fe3ceb31c74c1ec35700f5976d0ddbd8f
Arthur Lacoste <fostin@ip-172-26-11-116.eu-west-3.compute.internal>
2020-10-09 17:54:42 +0000

    retrying travis

--- projet8/travis.py
+++ projet8/travis.py
@@ -1,12 +1,18 @@
-from . import *
+import os
+
+"""
+Django settings file for you project.
+"""
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': '',
-        'USER': 'postgres',
-        'PASSWORD': '',
-        'HOST': '',
-        'PORT': '',
-    },
+    'ENGINE': 'django.db.backends.postgresql',
+    'NAME': os.environ['DATABASE_NAME'],
+    'USER': os.environ['DATABASE_USER'],
+    'PASSWORD': os.environ['DATABASE_PASSWORD'],
+    }
 }
+
+DATABASE_NAME = 'test_db'
+DATABASE_USER = 'postgres'
+DATABASE_PASSWORD = ''
